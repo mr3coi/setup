9c28f8729850f3731b1bd2299553948a6f507a36
SJC <mr3coi@gmail.com>
2020-04-06 22:35:24 -0400

    [Adds] 'git adog' alias

--- setup_git.sh
+++ setup_git.sh
@@ -4,3 +4,6 @@ git config --global user.name SJC
 git config --global user.email mr3coi@gmail.com
 
 git config --global push.default simple
+
+# Aliases
+git config --global alias.adog 'log --all --decorate --oneline --graph'
