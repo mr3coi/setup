8fdc557950be48c218aed54f36b60161770a9226
SJC <mr3coi@gmail.com>
2020-04-24 13:25:03 -0400

    [Adds] auto colorization to config

--- setup_git.sh
+++ setup_git.sh
@@ -4,6 +4,7 @@ git config --global user.name SJC
 git config --global user.email mr3coi@gmail.com
 
 git config --global push.default simple
+git config --global color.ui auto	# Colorize git outputs
 
 # Aliases
 git config --global alias.adog 'log --all --decorate --oneline --graph'
