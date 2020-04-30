536fc7bfab33b8bee2a837eee7b7701e0f8fb262
SJC <mr3coi@gmail.com>
2020-04-30 14:02:35 -0400

    [Adds] git clog

--- setup_git.sh
+++ setup_git.sh
@@ -7,3 +7,4 @@ git config --global push.default simple
 
 # Aliases
 git config --global alias.adog 'log --all --decorate --oneline --graph'
+git config --global alias.clog "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
